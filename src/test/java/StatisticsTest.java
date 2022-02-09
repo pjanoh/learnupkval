c27cf831f2ff11aded111dadd0ac9b363c74c6bf
Filipp Voronov <fivoronov@gmail.com>
2022-02-09 21:10:36 +0300

    Fix test

--- src/test/java/StatisticsTest.java
+++ src/test/java/StatisticsTest.java
@@ -5,10 +5,10 @@ public class StatisticsTest {
 
     @Test
     public void shouldFindMaxInTheMiddle() {
-        int[] salaries = { 15_000, 5_000, 25_000, 17_000 };
+        int[] salaries = { 35_000, 5_000, 25_000, 17_000 };
         Statistics st = new Statistics(salaries);
 
-        int expected = 2_000;
+        int expected = 35_000;
         int actual = st.max();
 
         Assertions.assertEquals(expected, actual);
